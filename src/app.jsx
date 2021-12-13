a797c5ac3a12906cf6ba2b2b01f685f9f108082b
Tom Soulard <tom.soulard@enclave.io>
2021-12-13 20:20:18 +0000

    Moved table to it's own component

--- src/app.jsx
+++ src/app.jsx
@@ -20,6 +20,7 @@
 import cockpit from 'cockpit';
 import React from 'react';
 import { Alert, Card, CardTitle, CardBody, Brand, Spinner } from '@patternfly/react-core';
+import PeerTable from './PeerTable';
 
 let details = {};
 
@@ -29,8 +30,6 @@ export class Application extends React.Component {
         this.setState({details: {
             peers: []
         }}); 
-        // this.state = { hostname: _("Unknown") };
-
         // cockpit.file('/etc/hostname').watch(content => {
         //     this.setState({ hostname: content.trim() });
         // });
@@ -52,56 +51,7 @@ export class Application extends React.Component {
                         </CardBody>
                     </Card>
 
-                    <TableComposable aria-label="Misc table">
-      <Thead noWrap>
-        <Tr>
-          <Th>{columns[0]}</Th>
-          <Th>{columns[1]}</Th>
-          <Th>{columns[2]}</Th>
-          <Th>{columns[3]}</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {rows.map((row, rowIndex) => {
-          const isOddRow = (rowIndex + 1) % 2;
-          const customStyle = {
-            borderLeft: '3px solid var(--pf-global--primary-color--100)'
-          };
-          return (
-            <Tr
-              key={rowIndex}
-              className={isOddRow ? 'odd-row-class' : 'even-row-class'}
-              style={isOddRow ? customStyle : {}}
-            >
-              {row.map((cell, cellIndex) => {
-                if (!cell) {
-                  return null;
-                }
-                let cellObject;
-                if (typeof cell !== 'object') {
-                  cellObject = {
-                    title: cell
-                  };
-                } else {
-                  cellObject = cell;
-                }
-                const { title, ...rest } = cellObject;
-                return (
-                  <Td
-                    key={`${rowIndex}_${cellIndex}`}
-                    dataLabel={columns[cellIndex]}
-                    textCenter={cellIndex === row.length - 1}
-                    {...rest}
-                  >
-                    {title}
-                  </Td>
-                );
-              })}
-            </Tr>
-          );
-        })}
-      </Tbody>
-    </TableComposable>
+                    <PeerTable peers={this.state.peers} />
                 </div>                
             )
         }
