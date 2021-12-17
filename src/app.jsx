59b0677f624dbbe260dfec22ac59b0cd2719f23a
Thomas Soulard <tom.soulard@enclave.io>
2021-12-17 00:08:49 +0000

    removed lib and now have a working react app with help from @psmorgan

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import cockpit from "cockpit";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardTitle,
@@ -15,68 +15,58 @@ import {
 } from "@patternfly/react-core";
 import PeerTable from './peertable.jsx'
 
-export default class Application extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-    };
-
-    setInterval(function() {
-      this.getStatus();
-    }, 2000).bind(this);
-  }
-
-  getStatus() {
-    cockpit
-    .spawn(["enclave", "status", "--json"])
-    .then(data => {
-      var statusDetails = JSON.parse(data);
-      this.setState({ details: statusDetails });
-    })
-    .catch(exception => {
-      console.log(exception);
-    }).bind(this);
-  }
-
-  render() {
-    if (this.state.details == null) {
-      return <Spinner isSVG />;
-    } else {
-      const Header = (
-        <Masthead display={{ default: "stack" }} inset={{ default: "insetXs" }}>
-          <MastheadMain>
-            <MastheadBrand
-              href="https://patternfly.org"
-              onClick={() => console.log("clicked logo")}
-              target="_blank"
-            >
-              Logo
-            </MastheadBrand>
-          </MastheadMain>
-        </Masthead>
-      );
-
-      return (
-        <Page header={Header}>
-          <PageSection variant={PageSectionVariants.light}>
-            Section with light background
-          </PageSection>
-
-          {/* <Brand src={pfLogo} alt="Patternfly Logo" /> */}
-
-          <Card>
-            <CardTitle>
-              Client Peers
-            </CardTitle>
-            <CardBody>
-              <PeerTable details={this.state.details} />
-            </CardBody>
-          </Card>
 
 
-        </Page>
-      );
-    }
+const getStatus = () => cockpit.spawn(["enclave", "status", "--json"]).then(JSON.parse);
+
+export default function Application() {
+  const [status, setStatus] = useState(undefined);
+  const [hasErrored, setHasErrored] = useState(false);
+
+
+  useEffect(() => {
+    setInterval(() => {
+      getStatus()
+        .then(result => setStatus(result))
+        .catch(err => {
+          console.log(err);
+          setHasErrored(true);
+        });
+    }, 2000);
+  }, []);
+
+  if (!status) {
+    return <Spinner isSVG />;
+  } else {
+    return (
+      <Page header={Header}>
+        <PageSection variant={PageSectionVariants.light}>
+          Section with light background
+        </PageSection>
+
+        <Card>
+          <CardTitle>
+            Client Peers
+          </CardTitle>
+          <CardBody>
+            <PeerTable status={status} />
+          </CardBody>
+        </Card>
+      </Page>
+    );
   }
 }
+
+const Header = (
+  <Masthead display={{ default: "stack" }} inset={{ default: "insetXs" }}>
+    <MastheadMain>
+      <MastheadBrand
+        href="https://patternfly.org"
+        onClick={() => console.log("clicked logo")}
+        target="_blank"
+      >
+        Logo
+      </MastheadBrand>
+    </MastheadMain>
+  </Masthead>
+);
--- src/peertable.jsx
+++ src/peertable.jsx
@@ -12,9 +12,31 @@ import { Spinner } from "@patternfly/react-core";
 
 
 export default function PeerTable(props) {
-  if (props == null || props.details == null) {
+  if (!props.status.Peers) {
     return <Spinner />;
   } else {
+    // Remove Discovery Service
+    var tableRows = props.status.Peers.map((peer, rowIndex) => {
+      const isOddRow = (rowIndex + 1) % 2;
+      const customStyle = {
+        borderLeft: "3px solid var(--pf-global--primary-color--100)",
+      };
+      return (
+        <Tr
+          key={rowIndex}
+          className={isOddRow ? "odd-row-class" : "even-row-class"}
+          style={isOddRow ? customStyle : {}}
+        >
+          <Td>{peer.name}</Td>
+          <Td>{peer.Description}</Td>
+          <Td>{peer.Tunnel ? peer.Tunnel.ProtocolEndPoint : "Down"}</Td>
+          <Td>{peer.RoundTripTime ?? ""}</Td>
+          <Td>{peer.VirtualAddress ?? ""}</Td>
+        </Tr>
+      );
+    }
+    );
+
     return (
       <TableComposable aria-label="Peers Table">
         <Caption>Peers</Caption>
@@ -28,25 +50,7 @@ export default function PeerTable(props) {
           </Tr>
         </Thead>
         <Tbody>
-          {this.state.details.peers.map((peer, rowIndex) => {
-            const isOddRow = (rowIndex + 1) % 2;
-            const customStyle = {
-              borderLeft: "3px solid var(--pf-global--primary-color--100)",
-            };
-            return (
-              <Tr
-                key={rowIndex}
-                className={isOddRow ? "odd-row-class" : "even-row-class"}
-                style={isOddRow ? customStyle : {}}
-              >
-                <Td>{peer.name}</Td>
-                <Td>{peer.Description}</Td>
-                <Td>{peer.Tunnel.ProtocolEndPoint ?? "Down"}</Td>
-                <Td>{peer.RoundTripTime ?? ""}</Td>
-                <Td>{peer.VirtualAddress ?? ""}</Td>
-              </Tr>
-            );
-          })}
+          {tableRows}
         </Tbody>
       </TableComposable>
     );
