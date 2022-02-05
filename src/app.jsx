847cca931d5b2e27ad16ee1acc804c4cac06b05f
Thomas Soulard <tom.soulard@enclave.io>
2022-02-05 22:29:43 +0000

    added url prefix to paths

--- src/app.jsx
+++ src/app.jsx
@@ -8,13 +8,15 @@ import Home from "./home.jsx";
 import Enrol from "./enrol.jsx";
 import NotRunning from "./not-running.jsx";
 
+
 export default function App () {
+    let urlPrefix = ':id/enclave/index.html';
     return(
-        <BrowserRouter basename='/cockpit/@localhost/enclave/index.html'>
+        <BrowserRouter basename='/cockpit/'>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="enrol" element={<Enrol />} />
-            <Route path="not-running" element={<NotRunning />} />
+            <Route path={`${urlPrefix}/`} element={<Home />} />
+            <Route path={`${urlPrefix}/enrol`} element={<Enrol />} />
+            <Route path={`${urlPrefix}/not-running`} element={<NotRunning />} />
           </Routes>
         </BrowserRouter>
       );
