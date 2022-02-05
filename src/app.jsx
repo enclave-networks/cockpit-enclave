dc4f9ea7379281e6f8024501e8808c9a5e2616d5
Thomas Soulard <tom.soulard@enclave.io>
2022-02-05 22:36:17 +0000

    cleanup code

--- src/app.jsx
+++ src/app.jsx
@@ -8,16 +8,15 @@ import Home from "./home.jsx";
 import Enrol from "./enrol.jsx";
 import NotRunning from "./not-running.jsx";
 
-
-export default function App () {
-    let urlPrefix = ':id/enclave/index.html';
-    return(
-        <BrowserRouter basename='/cockpit/'>
-          <Routes>
-            <Route path={`${urlPrefix}/`} element={<Home />} />
-            <Route path={`${urlPrefix}/enrol`} element={<Enrol />} />
-            <Route path={`${urlPrefix}/not-running`} element={<NotRunning />} />
-          </Routes>
-        </BrowserRouter>
-      );
+export default function App() {
+  let urlPrefix = ':id/enclave/index.html';
+  return (
+    <BrowserRouter basename='/cockpit/'>
+      <Routes>
+        <Route path={`${urlPrefix}/`} element={<Home />} />
+        <Route path={`${urlPrefix}/enrol`} element={<Enrol />} />
+        <Route path={`${urlPrefix}/not-running`} element={<NotRunning />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
